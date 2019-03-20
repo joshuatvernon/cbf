e0efe335462542a2d316b4846ce8f39ec0ed798d
Joshua Vernon <jvernon@atlassian.com>
2019-03-20 19:17:19 +1100

    1.0.2

--- src/cli/index.js
+++ src/cli/index.js
@@ -138,7 +138,7 @@ const addOperationsToCommander = () => {
 };
 
 const init = () => {
-    commander.version('1.0.1');
+    commander.version('1.0.2');
     commander.usage('[options]');
     addOperationsToCommander();
     commander.parse(process.argv);
