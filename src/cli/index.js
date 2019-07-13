0d72f18059045e49d3ad4bf875e1ec52921080c3
Joshua Vernon <joshuatvernon@gmail.com>
2019-07-13 18:48:33 +1000

    Updated commander version

--- src/cli/index.js
+++ src/cli/index.js
@@ -131,7 +131,7 @@ const addOperationsToCommander = () => {
 };
 
 const init = () => {
-  commander.version('1.1.3');
+  commander.version(process.env.npm_package_version);
   commander.usage('[options]');
   addOperationsToCommander();
   commander.parse(process.argv);
